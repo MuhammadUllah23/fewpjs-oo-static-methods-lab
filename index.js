da785547f2728e926b655bc306654f4ecad2f5a1
Muhammad <muhammadhullah23@gmail.com>
2021-09-25 16:56:11 -0400

    commit tree c7ed2d563b11c746fd8bccd9c43c6af87c7db3a2

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize( string ) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize( string ) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize( sentence ) {
+    const exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let title = [];
+    let titleArray = sentence.split(" ")
+    for ( let x = 0; x < titleArray.length; x++ ) {
+      if ( x == 0 ) {
+        title.push( this.capitalize( titleArray[ x ] ) )
+      } else {
+        if ( exceptions.includes( titleArray[ x ] ) ) {
+          title.push( titleArray[ x ] )
+        } else {
+          title.push( this.capitalize( titleArray[ x ] ) )
+        }
+      }
+    }
+    return title.join(" ");
+  }
 }
